937d51f47cb851828493a7f22e300327a5983be0
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-18 01:52:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,7 +2,8 @@ def prime?(num)
     if num < 0 || num = 0 || num = 1
    return false
     else
-       (2.. num - 1).to_a.all? {|new_num| new_num != 0 }
-       return true
+       (2.. num - 1).to_a.all? do |new_num|#{|new_num| new_num != 0 }
+      num % new_num != 0 #return true
+    end
     end
 end
